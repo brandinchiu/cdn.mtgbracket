7fe387ba046e55239ec05d18d30a32953b0cc6e8
Brandin Chiu <brandinchiu@gmail.com>
2016-08-28 19:16:58 -0400

    update versions and added console framework.

---
+++ web/console.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Project: cdn.mtgbracket.
+ * User: Brandin
+ * Date: 8/28/2016
+ * Time: 7:02 PM
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . '/../config/parameters.yml'));
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . '/../config/config.yml', $app['parameters']));
+$app->register(new Knp\Provider\ConsoleServiceProvider(), [
+    'console.name' => 'cdn.mtgbracket.console',
+    'console.version' => '1.0.0',
+    'console.project_directory' => __DIR__ . '/..'
+]);
+
+$console = $app['console'];
+
+
+
+$console->run();
--- web/index.php
+++ web/index.php
@@ -6,39 +6,63 @@
  * Time: 9:34 AM
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception;
 
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
-
 $app['debug'] = true;
 
 /**
- * error handler
+ * error handler.
  */
-$app->error(function(\Exception $e, $code){
+$app->error(function(\Exception $e) use ($app){
+    $code = $e->getCode();
     $response = new Response();
 
     switch($code){
         case Response::HTTP_NOT_FOUND:
+            /**
+             * TODO: provide default 404 image.
+             */
             $message = 'not found';
             $response->setStatusCode(Response::HTTP_NOT_FOUND);
 
             break;
         default:
             $message = 'error';
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
 
             break;
     }
 
-    $response->setContent($message);
+    /**
+     * build debug content.
+     */
+    $content = [
+        'error' => [
+            'code' => $code,
+            'message' => ($app['debug']) ? $e->getMessage() : $message,
+        ]
+    ];
+
+    if($app['debug']){
+        $content['error']['trace'] =  [
+            'file' => $e->getFile(),
+            'line' => $e->getLine()
+        ];
+    }
+
+    $response->setStatusCode($code);
+    $response->setContent(json_encode($content));
 
     return $response;
 });
+
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . '/../config/parameters.yml'));
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . '/../config/config.yml', $app['parameters']));
 
 /**
  * GET root
